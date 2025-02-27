4dc4aed31786af355f33bb95a30692df195ae8bb
Justin Hong <jjhong922@berkeley.edu>
2025-02-26 23:01:07 -0500

    redirect in browser

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,13 +5,15 @@
 
 // Create redirects
 exports.createPages = ({ actions }) => {
-  const { createRedirect } = actions;
-  
+  const { createRedirect } = actions
+
   // Redirect /workout to the deployed workout tracker app
   createRedirect({
-    fromPath: '/workout',
-    toPath: 'https://ai-workout-tracker-justjhong-justjhongs-projects.vercel.app',
+    fromPath: "/workout",
+    toPath:
+      "https://ai-workout-tracker-justjhong-justjhongs-projects.vercel.app",
+    redirectInBrowser: true,
     isPermanent: true,
     force: true,
-  });
-};
+  })
+}
